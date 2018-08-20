5863c5515ceafb862188946de0c1e3fcead05fd7
Justin Reich <justinreich.dev@gmail.com>
2018-08-20 09:50:46 -0500

    Solution to servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,22 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        PrintWriter out = null;
+        String name = (req.getParameter("name") != null) ? req.getParameter("name") : "World";
 
+
+        try {
+            out = res.getWriter();
+            out.printf("<h1>Hello, %s!</h1>", name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
